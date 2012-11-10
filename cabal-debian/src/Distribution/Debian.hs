3d092f943e919cf199383bca5b7068c4734e3e3d
David Fox <dsf@seereason.com>
2012-11-10 19:08:33 +0000

    Add some cabal-debian documentation

--- cabal-debian/src/Distribution/Debian.hs
+++ cabal-debian/src/Distribution/Debian.hs
@@ -3,6 +3,32 @@
 -- the cabal-debian executable, and to allow calls to the two top
 -- level functions that cabal-debian calls: 'debianize' and
 -- 'substvar'.
+-- 
+-- QUICK START: You can either call the cabal-debian executable, which
+-- is how the autobuilder debianizes library packages, or you can
+-- modify a package's Setup.hs file to call autobuilderDebianize for
+-- more sophisticated debianization.  This normally looks like a call
+-- to autobuilderDebianize:
+-- 
+-- > import Distribution.Debian (Flags(..), defaultFlags, autobuilderDebianize,
+-- >                             Executable(..), Server(..), Site(..))
+-- > import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
+-- > 
+-- > main = defaultMainWithHooks simpleUserHooks { postConf = \ _ _ _ lbi -> autobuilderDebianize lbi flags }
+-- 
+-- Now define flags:
+-- 
+-- ^ flags :: Flags
+-- ^ flags = defaultFlags { ... }
+-- 
+-- To see what your debianization would produce run
+-- 
+-- > runhaskell Setup configure
+-- 
+-- To actually create the files, and then build:
+-- 
+-- > runhaskell Setup configure --builddir=debian
+-- > dpkg-buildpackage
 module Distribution.Debian
     ( debianize
     , autobuilderDebianize
