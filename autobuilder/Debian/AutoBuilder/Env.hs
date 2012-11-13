79efceb82b2dc53c684330dd85744339964fd807
David Fox <dsf@seereason.com>
2012-11-13 18:51:14 +0000

    Remove code that attempts to symlink to the old build environments instead of building new ones - it doesn't work

--- autobuilder/Debian/AutoBuilder/Env.hs
+++ autobuilder/Debian/AutoBuilder/Env.hs
@@ -9,24 +9,19 @@ import Control.Monad.Trans (MonadIO, liftIO)
 import Debian.Release (ReleaseName, releaseName')
 import Debian.Repo (EnvRoot(EnvRoot))
 import Debian.Repo.Monads.Top (MonadTop, sub)
-import System.Directory (doesDirectoryExist)
 import System.FilePath ((</>))
-import System.Posix.Files (createSymbolicLink)
 
 buildEnvOfRelease :: (MonadIO m, MonadTop m) => ReleaseName -> m EnvRoot
 buildEnvOfRelease distro =
-    do maybeCreateLink distro "build-Lax" "build"
-       sub ("dists" </> releaseName' distro </> "build") >>= return . EnvRoot
+    sub ("dists" </> releaseName' distro </> "build") >>= return . EnvRoot
 
 dependEnvOfRelease :: (MonadIO m, MonadTop m) => ReleaseName -> m EnvRoot
 dependEnvOfRelease distro =
-    do maybeCreateLink distro "clean-Lax" "depend"
-       sub ("dists" </> releaseName' distro </> "depend") >>= return . EnvRoot
+    sub ("dists" </> releaseName' distro </> "depend") >>= return . EnvRoot
 
 cleanEnvOfRelease :: (MonadIO m, MonadTop m) => ReleaseName -> m EnvRoot
 cleanEnvOfRelease distro =
-    do maybeCreateLink distro "clean-Moderate" "clean"
-       sub ("dists" </> releaseName' distro </> "clean") >>= return . EnvRoot
+    sub ("dists" </> releaseName' distro </> "clean") >>= return . EnvRoot
 
 buildEnv :: (MonadIO m, MonadTop m) => ReleaseName -> m EnvRoot
 buildEnv distro = buildEnvOfRelease distro
@@ -36,11 +31,3 @@ dependEnv distro = dependEnvOfRelease distro
 
 cleanEnv :: (MonadIO m, MonadTop m) => ReleaseName -> m EnvRoot
 cleanEnv distro = cleanEnvOfRelease distro
-
-maybeCreateLink :: (MonadIO m, MonadTop m) => ReleaseName -> String -> String -> m ()
-maybeCreateLink distro old new =
-    do newExists <- liftIO $ doesDirectoryExist new
-       when (not newExists)
-            (do path <- sub ("dists" </> releaseName' distro </> old)
-                exists <- liftIO $ doesDirectoryExist path
-                when exists (liftIO $ createSymbolicLink new path))
