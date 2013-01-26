0fe78c90a4ae0bc0d40c24cf6751f25634e5c9f5
David Fox <dsf@seereason.com>
2013-01-26 00:23:02 +0000

    Add a Debian.Debianize module that re-exports everything in cabal-debian

---
+++ cabal-debian/src/Debian/Debianize.hs
@@ -0,0 +1,47 @@
+module Debian.Debianize
+    ( module Debian.Debianize.Atoms
+    , module Debian.Debianize.Bundled
+    , module Debian.Debianize.Cabal
+    , module Debian.Debianize.Combinators
+    , module Debian.Debianize.Debianize
+    , module Debian.Debianize.Dependencies
+    , module Debian.Debianize.Files
+    , module Debian.Debianize.Flags
+    , module Debian.Debianize.Generic
+    , module Debian.Debianize.Input
+    , module Debian.Debianize.Interspersed
+    , module Debian.Debianize.Output
+    , module Debian.Debianize.Server
+    , module Debian.Debianize.SubstVars
+    , module Debian.Debianize.Tests
+    , module Debian.Debianize.Types.Atoms
+    , module Debian.Debianize.Types.Debianization
+    , module Debian.Debianize.Types.Dependencies
+    , module Debian.Debianize.Types.PackageHints
+    , module Debian.Debianize.Types.PackageType
+    , module Debian.Debianize.Utility
+    , module Debian.Policy
+    ) where
+
+import Debian.Debianize.Atoms
+import Debian.Debianize.Bundled
+import Debian.Debianize.Cabal
+import Debian.Debianize.Combinators hiding (buildDeps)
+import Debian.Debianize.Debianize
+import Debian.Debianize.Dependencies
+import Debian.Debianize.Files
+import Debian.Debianize.Flags
+import Debian.Debianize.Generic
+import Debian.Debianize.Input
+import Debian.Debianize.Interspersed
+import Debian.Debianize.Output
+import Debian.Debianize.Server
+import Debian.Debianize.SubstVars
+import Debian.Debianize.Tests
+import Debian.Debianize.Types.Atoms
+import Debian.Debianize.Types.Debianization hiding (depends, conflicts)
+import Debian.Debianize.Types.Dependencies
+import Debian.Debianize.Types.PackageHints
+import Debian.Debianize.Types.PackageType hiding (VersionSplits)
+import Debian.Debianize.Utility
+import Debian.Policy
