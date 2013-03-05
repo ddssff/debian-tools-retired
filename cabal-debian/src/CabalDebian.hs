15240e2ceed928b8e79445d75784318669e54040
David Fox <dsf@seereason.com>
2013-03-05 14:12:54 +0000

    Give CabalDebian a reasonable defaultAtoms value

--- cabal-debian/src/CabalDebian.hs
+++ cabal-debian/src/CabalDebian.hs
@@ -5,7 +5,24 @@
 -- function directly, many sophisticated configuration options cannot
 -- be accessed using the command line interface.
 
+import Data.Lens.Lazy (setL)
+import Data.Map as Map (fromList)
+import Data.Monoid (mempty)
+import Data.Version (Version(Version))
+import Debian.Debianize.Atoms (Atoms, epochMap)
 import Debian.Debianize.Debianize (cabalDebian)
+import Debian.Debianize.VersionSplits (mapCabal, splitCabal)
+import Distribution.Package (PackageName(PackageName))
 
 main :: IO ()
-main = cabalDebian
+main = cabalDebian defaultAtoms
+
+defaultAtoms :: Atoms
+defaultAtoms =
+    setL epochMap (fromList [(PackageName "HaXml", 1), (PackageName "HTTP", 1)]) .
+    splitCabal (PackageName "parsec") "parsec2" (Version [3] []) .
+    mapCabal (PackageName "parsec") "parsec3" .
+    splitCabal (PackageName "QuickCheck") "quickcheck1" (Version [2] []) .
+    mapCabal (PackageName "QuickCheck") "quickcheck2" .
+    mapCabal (PackageName "gtk2hs-buildtools") "gtk2hs-buildtools" $
+    mempty
