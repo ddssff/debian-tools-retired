ecd46ace86e6aa583d22a637e0d0a8cc08724b5a
David Fox <dsf@seereason.com>
2013-03-05 13:20:00 +0000

    cabal-debian-3.2.5

--- cabal-debian/src/Debian/Debianize/VersionSplits.hs
+++ cabal-debian/src/Debian/Debianize/VersionSplits.hs
@@ -10,6 +10,8 @@ import Debian.Debianize.Atoms (Atoms, debianNameMap)
 import Debian.Debianize.Types.VersionSplits (VersionSplits, makePackage, insertSplit)
 import Distribution.Package (PackageName)
 
+-- | Add a VersionSplits value to 'atoms' that maps the given cabal
+-- name 'pname' to the given debian name 'dname'.
 mapCabal :: PackageName -> String -> Atoms -> Atoms
 mapCabal pname dname atoms =
     modL debianNameMap (Map.alter f pname) atoms
@@ -18,6 +20,8 @@ mapCabal pname dname atoms =
       f Nothing = Just (makePackage dname)
       f (Just sp) = error $ "mapCabal - already mapped: " ++ show sp
 
+-- | Map the versions of 'pname' less than 'ver' to the given debian
+-- name 'ltname'.
 splitCabal :: PackageName -> String -> Version -> Atoms -> Atoms
 splitCabal pname ltname ver atoms =
     modL debianNameMap (Map.alter f pname) atoms
