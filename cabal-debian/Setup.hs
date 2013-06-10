689612096d6fc43815f786fbc8f43ca20fd58e4c
David Fox <dsf@seereason.com>
2013-06-10 16:13:05 +0000

    Remove unused dependencies, disable call to runTestScript in Setup.hs

--- cabal-debian/Setup.hs
+++ cabal-debian/Setup.hs
@@ -1,19 +1,15 @@
 #!/usr/bin/runhaskell
 
+import Control.Monad (when)
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
 import Distribution.Simple.Program
 import System.Cmd
 import System.Exit
 
-main = defaultMainWithHooks simpleUserHooks {
-         postBuild =
-             \ _ _ _ lbi ->
-                 case buildDir lbi of
-                   "dist-ghc/build" -> return ()
-                   _ -> runTestScript lbi
-       -- , runTests = \ _ _ _ lbi -> runTestScript lbi
-       }
+main = defaultMainWithHooks simpleUserHooks
+       {- { postBuild = \ _ _ _ lbi -> when (buildDir lbi /= "dist-ghc/build") (runTestScript lbi)
+          , runTests = \ _ _ _ lbi -> runTestScript lbi } -}
 
 runTestScript lbi =
     system (buildDir lbi ++ "/cabal-debian-tests/cabal-debian-tests") >>= \ code ->
