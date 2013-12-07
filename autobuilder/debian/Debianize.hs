fe5b03427c415cde3a7768cf7d77db771e82a2e4
David Fox <dsf@seereason.com>
2013-12-07 14:02:24 +0000

    More autobuilder changes for cabal-debian-4

--- autobuilder/debian/Debianize.hs
+++ autobuilder/debian/Debianize.hs
@@ -1,6 +1,8 @@
 import Debian.Changes (ChangeLog)
-import Debian.Debianize (changelog, copyright, debianize, DebT, depends, doExecutable, inputChangeLog, seereasonDefaultAtoms, sourceFormat, sourcePackageName)
-import Debian.Debianize.ControlFile hiding (depends)
+import Debian.Debianize (DebT, evalDebT, debianization, writeDebianization, newAtoms, inputChangeLog,
+                         seereasonDefaultAtoms, changelog, license, copyright, depends, doExecutable,
+                         sourceFormat, sourcePackageName)
+import Debian.Debianize.Prelude ((~=), (%=))
 import Debian.Debianize.Types (InstallFile(InstallFile, destDir, destName, execName, sourceDir), Top(Top))
 import Debian.Policy (SourceFormat(Native3))
 import Debian.Relation (BinPkgName(BinPkgName), Relation(Rel), SrcPkgName(SrcPkgName), VersionReq(EEQ))
@@ -8,34 +10,33 @@ import Debian.Version (buildDebianVersion)
 import Distribution.License (License(AllRightsReserved))
 import Prelude hiding (log)
 
-main =
-    do log <- inputChangeLog top
-       debianize top (customize log) seereasonDefaultAtoms
-    where
-      top = Top "."
+top :: Top
+top = Top "."
 
-customize :: ChangeLog -> DebT IO ()
-customize log =
-    do sourcePackageName (SrcPkgName "autobuilder")
-       copyright (Left AllRightsReserved)
-       sourceFormat Native3
-       changelog log
-       mapM (depends (BinPkgName "autobuilder"))
-            [ Rel (BinPkgName "ghc") Nothing Nothing
-            , Rel (BinPkgName "libghc-autobuilder-dev") (Just (EEQ (buildDebianVersion Nothing "${Source-Version}" Nothing))) Nothing
-            , Rel (BinPkgName "debootstrap") Nothing Nothing
-            , Rel (BinPkgName "rsync") Nothing Nothing
-            , Rel (BinPkgName "dupload") Nothing Nothing
-            , Rel (BinPkgName "darcs") Nothing Nothing
-            , Rel (BinPkgName "git") Nothing Nothing
-            , Rel (BinPkgName "tla") Nothing Nothing
-            , Rel (BinPkgName "mercurial") Nothing Nothing
-            , Rel (BinPkgName "subversion") Nothing Nothing
-            , Rel (BinPkgName "apt") Nothing Nothing
-            , Rel (BinPkgName "build-essential") Nothing Nothing
-            , Rel (BinPkgName "quilt") Nothing Nothing
-            , Rel (BinPkgName "curl") Nothing Nothing
-            , Rel (BinPkgName "debian-archive-keyring") Nothing Nothing
-            , Rel (BinPkgName "seereason-keyring") Nothing Nothing
-            , Rel (BinPkgName "ubuntu-keyring") Nothing Nothing ]
+main = evalDebT (debianization top seereasonDefaultAtoms customize >> writeDebianization top) newAtoms
+
+customize :: DebT IO ()
+customize =
+    do inputChangeLog top
+       sourcePackageName ~= Just (SrcPkgName "autobuilder")
+       license ~= Just AllRightsReserved
+       sourceFormat ~= Just Native3
+       mapM_ (\ rs -> depends (BinPkgName "autobuilder") %= (++ rs))
+             [ [[Rel (BinPkgName "ghc") Nothing Nothing]]
+             , [[Rel (BinPkgName "libghc-autobuilder-dev") (Just (EEQ (buildDebianVersion Nothing "${Source-Version}" Nothing))) Nothing]]
+             , [[Rel (BinPkgName "debootstrap") Nothing Nothing]]
+             , [[Rel (BinPkgName "rsync") Nothing Nothing]]
+             , [[Rel (BinPkgName "dupload") Nothing Nothing]]
+             , [[Rel (BinPkgName "darcs") Nothing Nothing]]
+             , [[Rel (BinPkgName "git") Nothing Nothing]]
+             , [[Rel (BinPkgName "tla") Nothing Nothing]]
+             , [[Rel (BinPkgName "mercurial") Nothing Nothing]]
+             , [[Rel (BinPkgName "subversion") Nothing Nothing]]
+             , [[Rel (BinPkgName "apt") Nothing Nothing]]
+             , [[Rel (BinPkgName "build-essential") Nothing Nothing]]
+             , [[Rel (BinPkgName "quilt") Nothing Nothing]]
+             , [[Rel (BinPkgName "curl") Nothing Nothing]]
+             , [[Rel (BinPkgName "debian-archive-keyring") Nothing Nothing]]
+             , [[Rel (BinPkgName "seereason-keyring") Nothing Nothing]]
+             , [[Rel (BinPkgName "ubuntu-keyring") Nothing Nothing]] ]
        doExecutable (BinPkgName "autobuilder") (InstallFile {execName = "autobuilder", sourceDir = Just ".", destDir = Nothing, destName = "autobuilder"})
