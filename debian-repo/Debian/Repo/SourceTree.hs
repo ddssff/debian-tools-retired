54be001db51096e537274b7afe6aa9b80f2f9c0a
David Fox <dsf@seereason.com>
2014-01-06 21:27:39 +0000

    Increase progress verbosity for dpkg-buildpackage

--- debian-repo/Debian/Repo/SourceTree.hs
+++ debian-repo/Debian/Repo/SourceTree.hs
@@ -40,7 +40,7 @@ import System.Exit (ExitCode(ExitFailure), ExitCode(ExitSuccess))
 import System.FilePath ((</>))
 import System.IO (hGetContents, IOMode(ReadMode), withFile)
 import System.Process (CmdSpec(..), CreateProcess(cwd, env, cmdspec), proc, readProcessWithExitCode, showCommandForUser)
-import System.Process.Progress (keepResult, timeTask)
+import System.Process.Progress (keepResult, timeTask, noisier)
 import System.Unix.Chroot (useEnv)
 import Text.PrettyPrint.ANSI.Leijen (pretty)
 
@@ -101,8 +101,9 @@ buildDebs noClean _twice setEnv buildTree status =
       env0 <- liftIO getEnvironment
       -- Set LOGNAME so dpkg-buildpackage doesn't die when it fails to
       -- get the original user's login information
-      let run cmd = timeTask . useEnv root forceList $ runProc (cmd {env = Just (modEnv (("LOGNAME", Just "root") : setEnv) env0),
-                                                                     cwd = dropPrefix root path})
+      let run cmd = timeTask . useEnv root forceList . noisier 3 $
+                    runProc (cmd {env = Just (modEnv (("LOGNAME", Just "root") : setEnv) env0),
+                                  cwd = dropPrefix root path})
       _ <- liftIO $ run (proc "chmod" ["ugo+x", "debian/rules"])
       let buildCmd = proc "dpkg-buildpackage" (concat [["-sa"],
                                                        case status of Indep _ -> ["-B"]; _ -> [],
